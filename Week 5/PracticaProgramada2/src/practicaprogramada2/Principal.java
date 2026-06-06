60a855d1fad2e7466b8f68cb03304dcd3b2c2df4
Randall Segura <yjime01@gmail.com>
2026-06-05 21:31:24 -0600

    Subo otro ejemplo

---
+++ Week 5/PracticaProgramada2/src/practicaprogramada2/Principal.java
@@ -0,0 +1,15 @@
+
+package practicaprogramada2;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Menu m= new Menu();
+        m.setVisible(true);
+        m.setLocationRelativeTo(null);
+        
+    }
+    
+}
