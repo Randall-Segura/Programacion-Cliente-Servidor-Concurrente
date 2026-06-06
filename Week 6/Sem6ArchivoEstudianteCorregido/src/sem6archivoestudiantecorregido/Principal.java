d3ae202de86e5239791aaf0c9546051f845761e4
Randall Segura <yjime01@gmail.com>
2026-06-06 10:37:56 -0600

    Archivo corregido

---
+++ Week 6/Sem6ArchivoEstudianteCorregido/src/sem6archivoestudiantecorregido/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sem6archivoestudiantecorregido;
+
+/**
+ *
+ * @author Randall Segura
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu m= new Menu();
+        m.setVisible(true);
+        m.setLocationRelativeTo(null);
+    }
+    
+}
